6f24a04023f02919ff301b0ddcae8048feecb4a5
Erin Hurlburt <77364422+erinhurlburt@users.noreply.github.com>
2022-11-30 20:01:36 +0000

    Add files via upload

---
+++ Service/BlogAuth.swift
@@ -0,0 +1,60 @@
+//
+//  BlogAuth.swift
+//  Blog
+//
+//  Created by Erin Hurlburt on 4/3/22.
+//
+
+import Foundation
+
+import FirebaseAuthUI
+import FirebaseEmailAuthUI
+
+//must implement ObservableObject to be an environment object
+//@Published var is equivalent to a @State var (if it changes, it will refresh the views that use it)
+//when its an environment object, use @Published instead of @State
+class BlogAuth: NSObject, ObservableObject, FUIAuthDelegate {
+    let authUI: FUIAuth? = FUIAuth.defaultAuthUI()
+
+    // Multiple providers can be supported! See: https://firebase.google.com/docs/auth/ios/firebaseui
+    let providers: [FUIAuthProvider] = [
+        FUIEmailAuth()
+    ]
+
+    @Published var user: User?
+
+    /**
+     * You might not have overriden a constructor in Swift before...well, here it is.
+     */
+    override init() {
+        super.init()
+
+        // Note that authUI is marked as _optional_. If things don’t appear to work
+        // as expected, check to see that you actually _got_ an authUI object from
+        // the Firebase library.
+        authUI?.delegate = self
+        authUI?.providers = providers
+    }
+
+    /**
+     * In another case of the documentation being somewhat behind the latest libraries,
+     * this delegate method:
+     *
+     *     func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?)
+     *
+     * …has been deprecated in favor of the one below.
+     */
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        if let actualResult = authDataResult {
+            user = actualResult.user
+        }
+    }
+
+    func signOut() throws {
+        try authUI?.signOut()
+
+        // If we get past the logout attempt, we can safely clear the user.
+        user = nil
+    }
+}
+
