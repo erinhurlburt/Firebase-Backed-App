b522edf5ca6198d4dac786be0d008acc1912b317
Erin Hurlburt <77364422+erinhurlburt@users.noreply.github.com>
2022-11-30 20:04:38 +0000

    Add files via upload

---
+++ Helpers/FavoriteButton.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteButton.swift
+//  Blog
+//
+//  Created by Erin Hurlburt on 4/3/22.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
