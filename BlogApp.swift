f86165b461754b021e8ae6d5e3882a364193dcbb
Erin Hurlburt <77364422+erinhurlburt@users.noreply.github.com>
2022-11-30 19:57:56 +0000

    Add files via upload

---
+++ BlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogApp.swift
+//  Blog
+//
+//  Created by Erin Hurlburt on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
